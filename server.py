45ed56b3ae3db4709c2c660e8560cda206812217
N DIVIJ <ndivij2004@gmail.com>
2025-04-07 11:48:06 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,35 @@
+import requests
+from typing import Any
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("crimedetect")
+
+@mcp.tool()
+async def get_research(message: str) -> str:
+    """
+    Get latest news about the recent crime detective news on various blockchains
+    
+    """
+    url = "https://api.messari.io/ai/v1/chat/completions"
+    payload = {
+        "messages": [
+            {"role": "user", "content": message}
+        ],
+        "verbosity": "verbose",
+        "response_format": "markdown",
+        "inline_citations": True,
+        "stream": False
+    }
+
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "X-MESSARI-API-KEY": "QvrP9BBhcAclgAGpqCZ-8Galz3GZyzfvMNFmVwNArHJSq4zd"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    
+    return response.text
+
+if __name__ == "__main__":
+    # Initialize and run the server
+    mcp.run(transport='stdio')
